f91117e31fa8b264cefc9b5c118efa8768d69652
Aeiric <447206803@qq.com>
2016-09-05 18:31:41 +0800

    first commit

---
+++ app/src/main/java/com/aeiric/library/TestUtil.java
@@ -0,0 +1,13 @@
+package com.aeiric.library;
+
+import android.util.Log;
+
+/**
+ * Created by Aeiric on 16-9-5.
+ */
+public class TestUtil {
+    public static void test() {
+        System.out.println("----test----");
+        Log.e("Test", "-----test----");
+    }
+}
